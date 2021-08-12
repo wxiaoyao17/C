7eeba6cf697ec79424d7a3ee51770ef69bc243fd
wanglei <wanglei@rongcard.com>
2021-08-12 11:18:17 +0800

    test: wechat id

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+char string[] = {0b01110100, 0b01100001, 0b01101110, 0b01111001, 0b01110101, 0b01100111, 0b01100001, 0b01101110, 0b01100111, 0b01011111, 0b01100011, 0b01101111, 0b01101101};
+
 // 此程序很简单，仅仅打印一个Hello World的字符串。
 int main(void)
 {
     printf("Hello World! \n");
+    printf("wechat: %s \n", string);
     return 0;
 }
